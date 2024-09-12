d5527c7cbce8c06ef9dc95d4c1ea2844b11b74cf
krishnaamit2709 <ak997486@gmail.com>
2024-09-12 10:25:28 +0000

    data type

--- 01.js
+++ 01.js
@@ -1,1 +1,10 @@
 console.log("hello world");
+var a=10
+var b="20"
+ console.log(a+b)
+ console.log(9-"5")
+ console.log(""+"")
+ console.log(""+"0")
+ console.log("12"-"19")
+ console.log("amit"-"kumar")
+ console.log(true-true)
